205a8efd5bb6f0844b0bf8f8a1aa3a814ebd38cb
nabinkjha <nabinkjha@gmail.com>
2022-02-17 11:39:36 +0000

    Added missing files

---
+++ Ecom.API/Controllers/OData/v2/AuthenticateController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using System.Threading.Tasks;
+using ECom.API.Controllers.OData;
+using ECom.API.Controllers.OData.Login;
+using ECom.API.Utilities;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ECom.API.Controllers.OData.v2
+{
+    public class AuthenticateController : ControllerBase
+    {
+        private IConfiguration _config;
+
+        public AuthenticateController(IConfiguration config)
+        {
+            _config = config;
+        }
+        [AllowAnonymous]
+        [HttpPost("v2/Authenticate")]
+        public IActionResult Login(UserLogin data)
+        {
+            IActionResult response = Unauthorized();
+            var user = AuthenticateUser(data);
+            if (data != null)
+            {
+                var tokenString = GenerateJSONWebToken(user);
+                response = Ok(new { Token = tokenString, Message = "Success" });
+            }
+            return response;
+        }
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [HttpGet("v2/GetAccessToken")]
+        public async Task<IEnumerable<string>> Get()
+        {
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+
+            return new string[] { accessToken };
+        }
+        private string GenerateJSONWebToken(UserLogin userInfo)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+              _config["Jwt:Issuer"],
+              null,
+              expires: DateTime.Now.AddMinutes(120),
+              signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private UserLogin AuthenticateUser(UserLogin login)
+        {
+            UserLogin user = null;
+           // await Task.Delay(0);
+            if (login.UserName == "odata")
+            {
+                user = new UserLogin { UserName = "odata", Password = "odata123" };
+            }
+            return user;
+        }
+        
+    }
+}
---
+++ Ecom.API/Controllers/OData/v2/BaseODataController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+
+namespace ECom.API.Controllers.OData.Login
+{
+    [Produces("application/json")]
+    [Authorize(AuthenticationSchemes =JwtBearerDefaults.AuthenticationScheme)]
+    public abstract class BaseODataController : ODataController
+    {
+
+    }
+}
