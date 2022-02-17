e41b483760a907d349c932c4250a03b7d6ed10bc
nabinkjha <nabinkjha@gmail.com>
2022-02-17 11:57:43 +0000

    Added missing files

---
+++ Ecom.API/Utilities/UserLogin.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECom.API.Utilities
+{
+    public class UserLogin
+    {
+        [Required]
+        [Key]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
